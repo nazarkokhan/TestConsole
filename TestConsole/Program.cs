2a6c2a32fbd689f8df4e5d54e54539862c1dfd55
nazarkokhanisraelit <nazar.kokhan@israelit.pro>
2021-09-18 15:07:36 +0300

    .

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -2,12 +2,37 @@
 
 namespace TestConsole
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Net.Http;
+
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
-            
-            Console.WriteLine("Hello World!");
+            var str = "wtf man";
+            int intValue;
+            Structure a = new Structure();
+            object ob = str;
+            Console.WriteLine(ob.ToString());
+
+            Console.ReadLine();
         }
+    }
+
+    public ref struct Structure
+    {
+        public int _a;
+
+        public Structure(int a)
+        {
+            _a = a;
+        }
+    }
+
+    public abstract class AbstractClass
+    {
+        
     }
 }
